daff5981175325aae14d5bcce6a8be12d058169d
simonmuia <simonmuia38@gmail.com>
2023-01-12 17:13:07 +0300

    Added Blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './article.css';
 
-function Article() {
+function Article({imgUrl, date, title}) {
   return (
-    <div>Article</div>
+    // Define Article item card
+    <div className='gpt3__blog-container_article'>
+      {/* Article card image */}
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt='blog'/>
+      </div>
+      {/* Article card description */}
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        {/* Article card link */}
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
